2e7de1eaa055240b9af18e14bb2861dc3bb5a144
VIGOVSKYI <vuv3000@ukr.net>
2023-02-07 00:19:28 +0200

    finale

--- src/components/MyContacts/MyContacts.jsx
+++ src/components/MyContacts/MyContacts.jsx
@@ -9,10 +9,7 @@ import styles from './my-contacts.module.css';
 
 export default class MyContacts extends Component {
   state = {
-    contacts: [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-    ],
+    contacts: [],
     filter: '',
   };
 
